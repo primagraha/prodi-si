4ab1c9d4868c9d6e5bd3013e40577462b7529fc4
wahyu firmansyah <whyfirmansyah69@gmail.com>
2023-08-28 10:36:18 +0700

    pertama kali commit, memasukan semua file tailwind.css

---
+++ dist/js/script.js
@@ -0,0 +1,58 @@
+
+    // ======= Sticky
+    window.onscroll = function () {
+        const ud_header = document.querySelector(".ud-header");
+        const sticky = ud_header.offsetTop;
+        const logo = document.querySelector(".header-logo");
+    
+        if (window.pageYOffset > sticky) {
+          ud_header.classList.add("sticky");
+        } else {
+          ud_header.classList.remove("sticky");
+        }
+    
+        // === logo change
+        if (ud_header.classList.contains("sticky")) {
+          logo.src = "dist/img/logo/LOGO UPGJD.png";
+        } else {
+          logo.src = "dist/img/logo/LOGO UPGJD.png";
+        }
+    
+      };
+    
+      // ===== responsive navbar
+      let navbarToggler = document.querySelector("#navbarToggler");
+      const navbarCollapse = document.querySelector("#navbarCollapse");
+    
+      navbarToggler.addEventListener("click", () => {
+        navbarToggler.classList.toggle("navbarTogglerActive");
+        navbarCollapse.classList.toggle("hidden");
+      });
+    
+      //===== close navbar-collapse when a  clicked
+      document
+        .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
+        .forEach((e) =>
+          e.addEventListener("click", () => {
+            navbarToggler.classList.remove("navbarTogglerActive");
+            navbarCollapse.classList.add("hidden");
+          })
+        );
+    
+      // ===== Sub-menu
+      const submenuItems = document.querySelectorAll(".submenu-item");
+      submenuItems.forEach((el) => {
+        el.querySelector("a").addEventListener("click", () => {
+          el.querySelector(".submenu").classList.toggle("hidden");
+        });
+      });
+    
+      // ===== Faq accordion
+      const faqs = document.querySelectorAll(".single-faq");
+      faqs.forEach((el) => {
+        el.querySelector(".faq-btn").addEventListener("click", () => {
+          el.querySelector(".icon").classList.toggle("rotate-180");
+          el.querySelector(".faq-content").classList.toggle("hidden");
+        });
+      });
+    
